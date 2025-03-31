93534dd8c4e96c2cf42073091f17ab622a409859
sash04ek <a.ovcharov@gmail.com>
2025-03-31 15:22:06 +0300

    Refactoring of error code functionality

--- Module.php
+++ Module.php
@@ -9,6 +9,7 @@
 namespace Aurora\Modules\MailChangePasswordFastpanelPlugin;
 
 use Aurora\Modules\Mail\Models\MailAccount;
+use Aurora\System\Notifications;
 
 /**
  * Allows users to change passwords on their email accounts in Fastpanel.
@@ -284,7 +285,7 @@ class Module extends \Aurora\System\Module\AbstractModule
             }
 
             if (!isset($oRes4->data->id)) {
-                throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Exceptions\Errs::UserManager_AccountNewPasswordUpdateError);
+                throw new \Aurora\System\Exceptions\ApiException(Notifications::CanNotChangePassword);
             }
         }
         return true;
